c9a027795dd473eae44fe06152045258f0997a7f
louishb7 <luishenriquebdf@gmail.com>
2026-07-18 10:36:27 -0300

    Reestruturação do site

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*==================== MENU SHOW & HIDDEN ====================*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== SCROLL ABOUT ANIMATION ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
+
+/*==================== MIXITUP FILTER PORTFOLIO ====================*/
+
+/* Active work */
+
+/*==================== EMAIL JS ====================*/
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
