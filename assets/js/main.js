496ac39b28323fe3785d7abcfed9171db01c8d64
louishb7 <luishenriquebdf@gmail.com>
2026-09-02 21:31:23 -0300

    chore: remove unused animation dependencies

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,25 +61,3 @@ if (navMenu && navToggle) {
     link.addEventListener("click", () => setMenuState(false));
   });
 }
-
-/*==================== SCROLL ABOUT ANIMATION ====================*/
-const prefersReducedMotion = window.matchMedia(
-  "(prefers-reduced-motion: reduce)",
-).matches;
-
-if (!prefersReducedMotion) {
-  gsap.registerPlugin(ScrollTrigger);
-
-  gsap.utils.toArray(".text-gradient").forEach((span) => {
-    gsap.to(span, {
-      backgroundSize: "100% 100%",
-      ease: "none",
-      scrollTrigger: {
-        trigger: span,
-        start: "top bottom",
-        end: "top center",
-        scrub: true,
-      },
-    });
-  });
-}
